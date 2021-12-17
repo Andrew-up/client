da85e4c165ece7f403cc3efd2e02a40274f8a0dc
andrew-up <aageew@bk.ru>
2021-12-17 10:10:03 +0300

    service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+
+const USER_API ='http://localhost:8091/api/user/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpclient:HttpClient) {
+  }
+
+  getUserById(id:Number): Observable<any>{
+    return  this.httpclient.get(USER_API +id);
+  }
+
+  getCurrentUser(): Observable<any> {
+    return this.httpclient.get(USER_API);
+  }
+
+  updateUser(user:any): Observable<any>{
+    return this.httpclient.post(USER_API+'update',user);
+  }
 }
